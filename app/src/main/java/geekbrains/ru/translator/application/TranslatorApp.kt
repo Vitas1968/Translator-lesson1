4fabd8b94799256cd7cb0d8207d847e7ff1b092e
Vitas1968 <dor1968@mail.ru>
2020-05-14 19:51:39 +0700

    передеkываем класс App

--- app/src/main/java/geekbrains/ru/translator/application/TranslatorApp.kt
+++ app/src/main/java/geekbrains/ru/translator/application/TranslatorApp.kt
@@ -1,16 +1,26 @@
 package geekbrains.ru.translator.application
 
+import android.app.Activity
 import android.app.Application
+import dagger.android.DispatchingAndroidInjector
+import dagger.android.HasActivityInjector
 import timber.log.Timber
+import javax.inject.Inject
 
-class TranslatorApp: Application() {
-    companion object {
-        lateinit var instance: TranslatorApp
+class TranslatorApp: Application() , HasActivityInjector {
+
+    @Inject
+    lateinit var dispatchingAndroidInjector: DispatchingAndroidInjector<Activity>
+
+    override fun activityInjector(): DispatchingAndroidInjector<Activity>? {
+        return dispatchingAndroidInjector
     }
+
     override fun onCreate() {
         super.onCreate()
-        instance = this
-        Timber.plant(Timber.DebugTree())
+        DaggerAppComponent.builder()
+            .application(this)
+            .build()
+            .inject(this)
     }
-
 }
