2bd6b9b4912b2dd2f0b0eabd3ea59b65ad526c34
Vitas1968 <dor1968@mail.ru>
2020-05-14 19:56:42 +0700

    парсер результатлв

---
+++ app/src/main/java/geekbrains/ru/translator/utils/SearchResultParser.kt
@@ -0,0 +1,47 @@
+package geekbrains.ru.translator.utils
+
+import geekbrains.ru.translator.model.data.DataModel
+import geekbrains.ru.translator.model.data.Meanings
+import geekbrains.ru.translator.model.data.SearchResult
+
+fun parseSearchResults(data: DataModel): DataModel {
+    val newSearchResults = arrayListOf<SearchResult>()
+    when (data) {
+        is DataModel.Success -> {
+            val searchResults = data.data
+            if (!searchResults.isNullOrEmpty()) {
+                for (searchResult in searchResults) {
+                    parseResult(searchResult, newSearchResults)
+                }
+            }
+        }
+    }
+
+    return DataModel.Success(newSearchResults)
+}
+
+private fun parseResult(searchResult: SearchResult, newSearchResults: ArrayList<SearchResult>) {
+    if (!searchResult.text.isNullOrBlank() && !searchResult.meanings.isNullOrEmpty()) {
+        val newMeanings = arrayListOf<Meanings>()
+        for (meaning in searchResult.meanings) {
+            if (meaning.translation != null && !meaning.translation.translation.isNullOrBlank()) {
+                newMeanings.add(Meanings(meaning.translation, meaning.imageUrl))
+            }
+        }
+        if (newMeanings.isNotEmpty()) {
+            newSearchResults.add(SearchResult(searchResult.text, newMeanings))
+        }
+    }
+}
+
+fun convertMeaningsToString(meanings: List<Meanings>): String {
+    var meaningsSeparatedByComma = String()
+    for ((index, meaning) in meanings.withIndex()) {
+        meaningsSeparatedByComma += if (index + 1 != meanings.size) {
+            String.format("%s%s", meaning.translation?.translation, ", ")
+        } else {
+            meaning.translation?.translation
+        }
+    }
+    return meaningsSeparatedByComma
+}
