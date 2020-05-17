2fa8a4f8bcbd8ce83edd5d74766e3ee0a8fed3f7
Vitas1968 <dor1968@mail.ru>
2020-05-18 00:21:03 +0700

    делаем функцию suspend и меняем тип возвращаемого значения

--- app/src/main/java/geekbrains/ru/translator/model/repository/RepositoryImplementation.kt
+++ app/src/main/java/geekbrains/ru/translator/model/repository/RepositoryImplementation.kt
@@ -7,7 +7,7 @@ import io.reactivex.Observable
 class RepositoryImplementation(private val dataSource: DataSource<List<SearchResult>>) :
     Repository<List<SearchResult>> {
 
-    override fun getData(word: String): Observable<List<SearchResult>> {
+    override suspend fun getData(word: String): List<SearchResult> {
         return dataSource.getData(word)
     }
 }
