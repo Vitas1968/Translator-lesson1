b3909acde5566a19d9f63c949a5b4a25f83c5ef4
Vitas1968 <dor1968@mail.ru>
2020-05-18 02:03:56 +0700

    подчистил

--- app/src/main/java/geekbrains/ru/translator/model/repository/RepositoryImplementation.kt
+++ app/src/main/java/geekbrains/ru/translator/model/repository/RepositoryImplementation.kt
@@ -2,7 +2,6 @@ package geekbrains.ru.translator.model.repository
 
 import geekbrains.ru.translator.model.data.SearchResult
 import geekbrains.ru.translator.model.datasource.DataSource
-import io.reactivex.Observable
 
 class RepositoryImplementation(private val dataSource: DataSource<List<SearchResult>>) :
     Repository<List<SearchResult>> {
