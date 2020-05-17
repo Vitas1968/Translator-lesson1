999978911069d9dbdd203db76b46ad0834ee3842
Vitas1968 <dor1968@mail.ru>
2020-05-18 01:49:48 +0700

    добавил suspend и изменил тип возвращаемого значения

--- app/src/main/java/geekbrains/ru/translator/model/datasource/RoomDataBaseImplementation.kt
+++ app/src/main/java/geekbrains/ru/translator/model/datasource/RoomDataBaseImplementation.kt
@@ -5,7 +5,7 @@ import io.reactivex.Observable
 
 class RoomDataBaseImplementation : DataSource<List<SearchResult>> {
 
-    override fun getData(word: String): Observable<List<SearchResult>> {
+    override suspend fun getData(word: String): List<SearchResult> {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 }
