9ed03a531e8aad62ad1074fee5e81dd24d070fef
Vitas1968 <dor1968@mail.ru>
2020-05-18 00:36:53 +0700

    корректируем название функции

--- app/src/main/java/geekbrains/ru/translator/model/datasource/ApiService.kt
+++ app/src/main/java/geekbrains/ru/translator/model/datasource/ApiService.kt
@@ -9,5 +9,5 @@ import retrofit2.http.Query
 interface ApiService {
 
     @GET("words/search")
-    fun search(@Query("search") wordToSearch: String): Deferred<List<SearchResult>>
+    fun searchAsync(@Query("search") wordToSearch: String): Deferred<List<SearchResult>>
 }
