ba0327dc8ae3f46858e3175d19a226c458d8c4e8
Vitas1968 <dor1968@mail.ru>
2020-05-20 22:00:27 +0700

    реализация RoomDataBaseImplementation

--- app/src/main/java/geekbrains/ru/translator/model/datasource/RoomDataBaseImplementation.kt
+++ app/src/main/java/geekbrains/ru/translator/model/datasource/RoomDataBaseImplementation.kt
@@ -1,10 +1,22 @@
 package geekbrains.ru.translator.model.datasource
 
+import geekbrains.ru.translator.model.data.DataModel
 import geekbrains.ru.translator.model.data.SearchResult
+import geekbrains.ru.translator.room.HistoryDao
+import geekbrains.ru.translator.utils.convertDataModelSuccessToEntity
+import geekbrains.ru.translator.utils.mapHistoryEntityToSearchResult
 
-class RoomDataBaseImplementation : DataSource<List<SearchResult>> {
+class RoomDataBaseImplementation(private val historyDao: HistoryDao) :
+    DataSourceLocal<List<SearchResult>> {
 
     override suspend fun getData(word: String): List<SearchResult> {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        val listHistoryEntity=historyDao.all()
+        return mapHistoryEntityToSearchResult(listHistoryEntity)
+    }
+
+    override suspend fun saveToDB(dataModel: DataModel) {
+        convertDataModelSuccessToEntity(dataModel)?.let {
+            historyDao.insert(it)
+        }
     }
 }
