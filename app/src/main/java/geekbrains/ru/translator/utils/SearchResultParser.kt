46d8dfea759840dee792df559783eb4ec1207953
Vitas1968 <dor1968@mail.ru>
2020-05-20 19:58:47 +0700

    конвертер значения в строку

--- app/src/main/java/geekbrains/ru/translator/utils/SearchResultParser.kt
+++ app/src/main/java/geekbrains/ru/translator/utils/SearchResultParser.kt
@@ -81,3 +81,16 @@ fun convertDataModelSuccessToEntity(dataModel: DataModel): HistoryEntity? {
         else -> null
     }
 }
+
+
+fun convertMeaningsToString(meanings: List<Meanings>): String {
+    var meaningsSeparatedByComma = String()
+    for ((index, meaning) in meanings.withIndex()) {
+        meaningsSeparatedByComma += if (index + 1 != meanings.size) {
+            String.format("%s%s", meaning.translation?.translation, ", ")
+        } else {
+            meaning.translation?.translation
+        }
+    }
+    return meaningsSeparatedByComma
+}
