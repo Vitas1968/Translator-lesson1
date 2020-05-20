8a5311cc4b32ec94ba5940c96bf2dd1f1ee41f72
Vitas1968 <dor1968@mail.ru>
2020-05-20 19:09:10 +0700

    создаем экран history

---
+++ app/src/main/java/geekbrains/ru/translator/view/history/HistoryInteractor.kt
@@ -0,0 +1,23 @@
+package geekbrains.ru.translator.view.history
+
+import geekbrains.ru.translator.model.data.DataModel
+import geekbrains.ru.translator.model.data.SearchResult
+import geekbrains.ru.translator.model.repository.Repository
+import geekbrains.ru.translator.model.repository.RepositoryLocal
+import geekbrains.ru.translator.viewmodel.Interactor
+
+class HistoryInteractor(
+    private val repositoryRemote: Repository<List<SearchResult>>,
+    private val repositoryLocal: RepositoryLocal<List<SearchResult>>
+) : Interactor<DataModel> {
+
+    override suspend fun getData(word: String, fromRemoteSource: Boolean): DataModel {
+        return DataModel.Success(
+            if (fromRemoteSource) {
+                repositoryRemote
+            } else {
+                repositoryLocal
+            }.getData(word)
+        )
+    }
+}
