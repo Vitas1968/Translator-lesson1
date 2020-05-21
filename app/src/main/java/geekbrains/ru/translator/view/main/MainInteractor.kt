f19ae1ba03fe5ea956c60168f93dc4e23aa17bc4
Vitas1968 <dor1968@mail.ru>
2020-05-21 20:13:54 +0700

    добавил RepositoryLocal

--- app/src/main/java/geekbrains/ru/translator/view/main/MainInteractor.kt
+++ app/src/main/java/geekbrains/ru/translator/view/main/MainInteractor.kt
@@ -4,11 +4,12 @@ import com.anikin.aleksandr.simplevocabulary.viewmodel.Interactor
 import geekbrains.ru.translator.model.data.DataModel
 import geekbrains.ru.translator.model.data.SearchResult
 import geekbrains.ru.translator.model.repository.Repository
+import geekbrains.ru.translator.model.repository.RepositoryLocal
 
 
 class MainInteractor(
     private val remoteRepository: Repository<List<SearchResult>>,
-    private val localRepository: Repository<List<SearchResult>>
+    private val localRepository: RepositoryLocal<List<SearchResult>>
 ) : Interactor<DataModel> {
 
     override suspend fun getData(word: String, fromRemoteSource: Boolean): DataModel {
