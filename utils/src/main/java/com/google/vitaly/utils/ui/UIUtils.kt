e5e50ad8dc73187d5688a49f7cdc2c94fedf7fcb
Vitas1968 <dor1968@mail.ru>
2020-05-25 21:17:13 +0700

    импорты

--- utils/src/main/java/com/google/vitaly/utils/ui/UIUtils.kt
+++ utils/src/main/java/com/google/vitaly/utils/ui/UIUtils.kt
@@ -1,8 +1,9 @@
-package geekbrains.ru.translator.utils.ui
+package com.google.vitaly.utils.ui
 
 import android.content.Context
 import androidx.appcompat.app.AlertDialog
-import geekbrains.ru.translator.R
+import com.google.vitaly.utils.R
+
 
 fun getStubAlertDialog(context: Context): AlertDialog {
     return getAlertDialog(context, null, null)
