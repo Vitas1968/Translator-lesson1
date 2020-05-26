da5df9d3ad30f0f3da57d57630f16e6a7a6dcdce
Vitas1968 <dor1968@mail.ru>
2020-05-26 19:56:41 +0700

    убрал ненужное

--- app/src/main/java/geekbrains/ru/translator/application/TranslatorApp.kt
+++ app/src/main/java/geekbrains/ru/translator/application/TranslatorApp.kt
@@ -2,9 +2,6 @@ package geekbrains.ru.translator.application
 
 
 import android.app.Application
-import geekbrains.ru.translator.di.application
-import geekbrains.ru.translator.di.historyScreen
-import geekbrains.ru.translator.di.mainScreen
 import org.koin.android.ext.koin.androidContext
 import org.koin.core.context.startKoin
 
