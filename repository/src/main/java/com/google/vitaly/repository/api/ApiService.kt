1fbc161ef18cd50c4563f92d5e03b57a8876c1d3
Vitas1968 <dor1968@mail.ru>
2020-06-01 21:46:28 +0700

    меняем модель данных

--- repository/src/main/java/com/google/vitaly/repository/api/ApiService.kt
+++ repository/src/main/java/com/google/vitaly/repository/api/ApiService.kt
@@ -1,6 +1,6 @@
 package com.google.vitaly.repository.api
 
-import com.google.vitaly.model.data.SearchResult
+import com.google.vitaly.model.data.dto.SearchResultDto
 import kotlinx.coroutines.Deferred
 import retrofit2.http.GET
 import retrofit2.http.Query
@@ -8,5 +8,5 @@ import retrofit2.http.Query
 interface ApiService {
 
     @GET("words/search")
-    fun searchAsync(@Query("search") wordToSearch: String): Deferred<List<SearchResult>>
+    fun searchAsync(@Query("search") wordToSearch: String): Deferred<List<SearchResultDto>>
 }
