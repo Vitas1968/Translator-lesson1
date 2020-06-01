3d414d6e32543547f7958610a11ef0ee4b3e25eb
Vitas1968 <dor1968@mail.ru>
2020-06-01 21:48:03 +0700

    меняем модель данных

--- model/src/main/java/com/google/vitaly/model/data/dto/MeaningsDto.kt
+++ model/src/main/java/com/google/vitaly/model/data/dto/MeaningsDto.kt
@@ -1,8 +1,10 @@
-package geekbrains.ru.model.data.dto
+package com.google.vitaly.model.data.dto
 
 import com.google.gson.annotations.SerializedName
+import geekbrains.ru.model.data.dto.TranslationDto
 
 class MeaningsDto(
     @field:SerializedName("translation") val translation: TranslationDto?,
-    @field:SerializedName("imageUrl") val imageUrl: String?
+    @field:SerializedName("imageUrl") val imageUrl: String?,
+    @field:SerializedName("previewUrl") val previewUrl: String?
 )
