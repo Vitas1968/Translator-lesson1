51833c3ceebb25af79f510a4110092f0a8b3c296
Vitas1968 <dor1968@mail.ru>
2020-06-01 21:39:34 +0700

    меняем модель данных и добавляем маппинг

--- app/src/main/java/geekbrains/ru/translator/view/main/MainInteractor.kt
+++ app/src/main/java/geekbrains/ru/translator/view/main/MainInteractor.kt
@@ -2,23 +2,25 @@ package geekbrains.ru.translator.view.main
 
 import com.anikin.aleksandr.simplevocabulary.viewmodel.Interactor
 import com.google.vitaly.model.data.DataModel
-import com.google.vitaly.model.data.SearchResult
+import com.google.vitaly.model.data.dto.SearchResultDto
 import com.google.vitaly.repository.Repository
 import com.google.vitaly.repository.RepositoryLocal
+import geekbrains.ru.translator.utils.mapSearchResultToResult
 
 
 class MainInteractor(
-    private val remoteRepository: Repository<List<SearchResult>>,
-    private val localRepository: RepositoryLocal<List<SearchResult>>
+    private val remoteRepository: Repository<List<SearchResultDto>>,
+    private val localRepository: RepositoryLocal<List<SearchResultDto>>
 ) : Interactor<DataModel> {
 
     override suspend fun getData(word: String, fromRemoteSource: Boolean): DataModel {
-        return DataModel.Success(
-            if (fromRemoteSource) {
-                remoteRepository
-            } else {
-                localRepository
-            }.getData(word)
-        )
+        val dataModel: DataModel
+        if (fromRemoteSource) {
+            dataModel = DataModel.Success(mapSearchResultToResult(remoteRepository.getData(word)))
+            localRepository.saveToDB(dataModel)
+        } else {
+            dataModel = DataModel.Success(mapSearchResultToResult(localRepository.getData(word)))
+        }
+        return dataModel
     }
 }
